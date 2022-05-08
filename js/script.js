ea0d3639ac28418b3e8abe772da2114da8c5a306
Marco-Gagliano <brazil_94@hotmail.it>
2022-05-08 17:20:22 +0200

    milestone-2/completato

--- js/script.js
+++ js/script.js
@@ -1,61 +1,72 @@
 /*
-Milestone 1:
-
-- Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-
-- Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+Milestone 1: COMPLETATO!
+Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse.
+Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto.
 
 Milestone 2
-Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
-Click sul contatto mostra la conversazione del contatto cliccato
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione.
+Click sul contatto mostra la conversazione del contatto cliccato.
 
 Milestone 3
-Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde.
 Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 
 Milestone 4
-Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina).
 
 Milestone 5 - opzionale
-Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
-Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti
+Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato.
+Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti.
 
 Procedete un Melestone alla volta fate i commiti seprata per ogni Milestone (ovviamente oltre a tutti quelli intermedi).
 
 Le “finiture” lasciatele alla fine e non state a impazzire se ad esempio le scrollbar non si visualizzano perfettamente o cose simili. Se ci sarà tempo ala fine si possono anche curare questi aspetti ma date la priorità a tutte le funzionalità richieste.
 
 Note:
-non è importante per ora che tutti gli elementi grafici siano presenti, l’importante che la struttura sia come da specifiche
-l’elenco degli utenti deve essere generato con un v-for e stampare almeno il nome e l’avatar (ultimo messaggio e data per ora possono essere fake)
-provate sia con tanti e pochi messaggi con messaggi lunghi o corti. La pagina non si deve mai spaccare
+non è importante per ora che tutti gli elementi grafici siano presenti, l’importante che la struttura sia come da specifiche.
+l’elenco degli utenti deve essere generato con un v-for e stampare almeno il nome e l’avatar (ultimo messaggio e data per ora possono essere fake).
+provate sia con tanti e pochi messaggi con messaggi lunghi o corti. La pagina non si deve mai spaccare.
 
 */
 const app = new Vue({
 
+  el: '#app',
+
   data: {
+
+    utenteAttivo: 0,
+    ricercaUtente: '',
+    nuovoMessaggio: '',
+
+    users: 
+      {
+        name: 'John Wick',
+        avatar: '_io',
+      },
+
     utenti: [
       {
-        name: 'Michele',
-        avatar: '_1',
-        visible: true,
-        messages: [
-          {
-          date: '10/01/2020 15:30:55',
-          message: 'Hai portato a spasso il cane?',
-          status: 'sent'
-          },
+      name: 'Michele',
+      avatar: '_1',
+      visible: true,
+      messages: [
+        {
+        date: '10/01/2020 15:30:55',
+        message: 'Hai portato a spasso il cane?',
+        status: 'sent'
+        },
 
-          {
-          date: '10/01/2020 15:50:00',
-          message: 'Ricordati di stendere i panni',
-          status: 'sent'
-          },
-          
-          {
-          date: '10/01/2020 16:15:22',
-          message: 'Tutto fatto!',
-          status: 'received'
-          },
+        {
+        date: '10/01/2020 15:50:00',
+        message: 'Ricordati di stendere i panni',
+        status: 'sent'
+        },
+        
+        {
+        date: '10/01/2020 16:15:22',
+        message: 'Tutto fatto!',
+        status: 'received'
+        },
       ]},
 
       {
@@ -80,8 +91,7 @@ const app = new Vue({
         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
         status: 'sent'
         }
-      ],
-      },
+      ]},
 
       {
       name: 'Samuele',
@@ -103,8 +113,7 @@ const app = new Vue({
         message: 'Ah scusa!',
         status: 'received'
         }
-      ],
-      },
+      ]},
 
       {
       name: 'Alessandro B.',
@@ -121,8 +130,7 @@ const app = new Vue({
         message: 'Si, ma preferirei andare al cinema',
         status: 'received'
         }
-      ],
-      },
+      ]},
 
       {
       name: 'Alessandro L.',
@@ -139,8 +147,7 @@ const app = new Vue({
         message: 'Va bene, stasera la sento',
         status: 'received'
         }
-      ],
-      },
+      ]},
 
       {
       name: 'Claudia',
@@ -162,8 +169,7 @@ const app = new Vue({
         message: 'Nessuna nuova, buona nuova',
         status: 'sent'
         }
-      ],
-      },
+      ]},
 
       {
       name: 'Federico',
@@ -180,8 +186,7 @@ const app = new Vue({
         message: 'Grazie per avermelo ricordato, le scrivo subito!',
         status: 'received'
         }
-      ],
-      },
+      ]},
 
       {
       name: 'Davide',
@@ -203,16 +208,13 @@ const app = new Vue({
         message: 'OK!!',
         status: 'received'
         }
-      ],
-      }
-  ]
-  
-
+      ]},
+    ]
   },
 
   methods: {
-
-  }
+  
+  
   
 
-});
+}},)
